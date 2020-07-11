d4e897432925c699ca79c23618f5732cd7deca2c
Anatolii <pranzpro@gmail.com>
2020-07-11 15:03:48 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom'
 
 import Layout from './hoc/Layout/Layout';
+
+import Auth from './containers/Auth/Auth';
+import QuizCreator from './containers/QuizCreator/QuizCreator';
+import QuizList from './containers/QuizList/QuizList';
 import Quiz from './containers/Quiz/Quiz';
+
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <Quiz/>
+		  <Switch>
+			  <Route path="/auth" component={Auth}/>
+			  <Route path="/quiz-creator" component={QuizCreator}/>
+			  <Route path="/quiz/:id"  component={Quiz}/>
+			  <Route path="/" component={QuizList}/>
+		  </Switch>
       </Layout>
     );
   }
--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,48 +1,50 @@
-import React from 'react'
-import Button from '../UI/Button/Button'
+import React from "react";
+import Button from "../UI/Button/Button";
+import { Link } from "react-router-dom";
 
-import classes from './FinishedQuiz.module.scss'
+import classes from "./FinishedQuiz.module.scss";
 
-const FinishedQuiz = props => {
-  const successCounter = Object
-    .keys(props.results)
-    .reduce((total, key) => {
-      if (props.results[key] === 'success') {
-        total++
-      }
+const FinishedQuiz = (props) => {
+  const successCounter = Object.keys(props.results).reduce((total, key) => {
+    if (props.results[key] === "success") {
+      total++;
+    }
 
-      return total
-    }, 0)
+    return total;
+  }, 0);
   return (
     <div className={classes.FinishedQuiz}>
       <ul>
-        {props
-          .quiz
-          .map((quizItem, index) => {
-            const cls = [
-              'fa',
-              props.results[quizItem.id] === 'error'
-                ? 'fa-times'
-                : 'fa-check',
-              classes[props.results[quizItem.id]]
-            ]
-            return (
-              <li key={index}>
-                <strong>{index + 1}</strong>.&nbsp; {quizItem.question}
-                <i className={cls.join(' ')}/>
-              </li>
-            )
-          })}
+        {props.quiz.map((quizItem, index) => {
+          const cls = [
+            "fa",
+            props.results[quizItem.id] === "error" ? "fa-times" : "fa-check",
+            classes[props.results[quizItem.id]],
+          ];
+          return (
+            <li key={index}>
+              <strong>{index + 1}</strong>.&nbsp; {quizItem.question}
+              <i className={cls.join(" ")} />
+            </li>
+          );
+        })}
       </ul>
 
-      <p>Правильно&nbsp;{successCounter}&nbsp;из&nbsp;{props.quiz.length}</p>
+      <p>
+        Правильно&nbsp;{successCounter}&nbsp;из&nbsp;{props.quiz.length}
+      </p>
 
       <div>
-        <Button onClick={props.onRetry} type={'primary'}>Повторить</Button>
-        <Button type={'success'}>Перейти в список тестов</Button>
+        <Button onClick={props.onRetry} type={"primary"}>
+          Повторить
+        </Button>
+
+        <Link to="/">
+          <Button type={"success"}>Перейти в список тестов</Button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FinishedQuiz
+export default FinishedQuiz;
